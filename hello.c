210dd015a094853b9242c9d6491f9c0f9c9395de
Monira Akter <akter.monira.cse@gmail.com>
2024-01-31 21:36:27 +0600

    Hello world program

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(){
+    printf("hello, duniya..");
+    printf("program run hocche.. \nbut header file er warning line ta jacche nah.. -_-");
+    int a = 10, b= 20;
+    int c = a + b;
+    printf("\n%d", c);
+
+}
