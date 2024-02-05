6ce2bb4265a6433baf6cb897b547b3d61c62f763
Monira Akter <akter.monira.cse@gmail.com>
2024-02-05 23:33:34 +0600

    output - https://prnt.sc/yBeGWxH7QUwZ

--- csc-184-prblm8.c
+++ csc-184-prblm8.c
@@ -1,10 +1,12 @@
 //Mossamot Monira Akter ; ID:23303121; Serial: 05
+//output - https://prnt.sc/yBeGWxH7QUwZ
 #include<stdio.h>
 
 int main()
 {
-    printf("Name: Mossamot Monira Akter\nID: 23303121\nSerial: 05\n");
     int days;
+    printf("Name: Mossamot Monira Akter\nID: 23303121\nSerial: 05\n");
+    
     printf("Enter Days: ");  // to find year, month, week, day
     scanf("%d", &days);
 
