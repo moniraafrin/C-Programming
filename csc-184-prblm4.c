d19ddd038c20f4de85e6ff46ecb09d5ee8c48956
Monira Akter <akter.monira.cse@gmail.com>
2024-02-05 20:03:31 +0600

    Fahrenheit

---
+++ csc-184-prblm4.c
@@ -0,0 +1,15 @@
+//Mossamot Monira Akter ; ID:23303121; Serial: 05
+#include<stdio.h>
+
+int main()
+{
+    printf("Name: Mossamot Monira Akter\nID: 23303121\nSerial: 05\n");
+
+    float centigrade = 24.5, fahrenheit;
+    fahrenheit = ((centigrade * 9)/5) + 32;
+
+    printf("Temperature in fahrenheit: %.2f\n", fahrenheit);
+
+    return 0;
+
+}
