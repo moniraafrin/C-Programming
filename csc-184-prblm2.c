05bf700e9bc96690a2c72cfeaa200f54f5cc716f
Monira Akter <akter.monira.cse@gmail.com>
2024-02-05 20:00:56 +0600

    circle's perimeter,area

---
+++ csc-184-prblm2.c
@@ -0,0 +1,17 @@
+//Mossamot Monira Akter ; ID:23303121; Serial: 05
+#include<stdio.h>
+#define PI 3.1416
+int main()
+{
+    printf("Name: Mossamot Monira Akter\nID: 23303121\nSerial: 05\n");
+
+    float radius = 5.50;
+    float perimeter = 2 * PI * radius;
+    float area = PI * radius * radius;
+
+    printf("Perimeter of the circle is: %.2f\nArea of the circle is: %.2f\n", perimeter, area);
+
+    return 0;
+
+}
+
