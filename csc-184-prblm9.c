edc9bb90de2bfa879c299be3fee434151834f9fb
Monira Akter <akter.monira.cse@gmail.com>
2024-02-05 22:47:51 +0600

    find out the large number

---
+++ csc-184-prblm9.c
@@ -0,0 +1,19 @@
+//Mossamot Monira Akter ; ID:23303121; Serial: 05
+#include<stdio.h>
+
+int main()
+{
+    printf("Name: Mossamot Monira Akter\nID: 23303121\nSerial: 05\n");
+    int num1,num2;
+    scanf("%d %d", &num1,&num2);
+    if(num1>num2)
+    {
+        printf("Num1 is large");
+    }
+    else
+    {
+        printf("Num2 is large");
+    }
+
+    return 0;
+}
