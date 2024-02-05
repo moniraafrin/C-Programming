031932759b32b48cf1c66f502b5236e22b2464a4
Monira Akter <akter.monira.cse@gmail.com>
2024-02-05 20:02:13 +0600

    find the Angle

---
+++ csc-184-prblm3.c
@@ -0,0 +1,14 @@
+//Mossamot Monira Akter ; ID:23303121; Serial: 05
+#include<stdio.h>
+#define PI 3.1416
+int main()
+{
+    printf("Name: Mossamot Monira Akter\nID: 23303121\nSerial: 05\n");
+    int angle1 = 45, angle2 = 45, angle3;
+    int total_angle = 180;
+    angle3 = total_angle - (angle1 + angle2);
+    printf("Third Angle is: %d\n", angle3);
+
+    return 0;
+
+}
