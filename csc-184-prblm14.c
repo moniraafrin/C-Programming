8c6fb0e5a61dd5dc309ba44852a6991bf5311ae2
Monira Akter <akter.monira.cse@gmail.com>
2024-02-11 11:08:19 +0600

    root

---
+++ csc-184-prblm14.c
@@ -0,0 +1,14 @@
+//Write a program to enter any number and calculate its square root.
+
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+printf("Name: Mossamot Monira Akter\nId: 23303121\nSerial: 05\n");
+ float num;
+ scanf("%f",&num);
+ float root = sqrt(num);
+ printf("Square root is: %.f\n", root);
+  return 0;
+}
