6ebb0667b2305568d5da53689828e6151088a2bd
Monira Akter <akter.monira.cse@gmail.com>
2024-02-11 10:55:03 +0600

    total salary

---
+++ csc-184-prblm12.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+printf("Name: Mossamot Monira Akter\nId: 23303121\nSerial: 05\n");
+    int employee_id, present_day;
+    float total_salary;
+    double salary_per_day;
+
+    printf("\nEnter the Employee id: ");
+     scanf("%d",&employee_id);
+
+    printf("Enter the number of days present in the month: ");
+     scanf("%d",&present_day);
+
+    printf("Enter the salary per day: ");
+     scanf("%lf",&salary_per_day);
+
+    total_salary = (present_day * salary_per_day);
+    printf("\nEnter the Employee id: %d\nTotal Salary = BDT %.2f\n",employee_id,total_salary);
+
+    return 0;
+
+}
