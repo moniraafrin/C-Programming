1583a6830cfb57b1cff4fa1c688031fc0b51aff8
Monira Akter <158304777+moniraafrin@users.noreply.github.com>
2024-03-17 13:02:00 +0600

    lowecase letter to uppercase value

---
+++ ascii-character-lowecase.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+
+    char lower;
+    printf("Enter any lowercase letter: ");
+    scanf("%c",&lower);
+    printf("The Uppercase letter is: %d\n",lower-32);
+
+return 0;
+
+}
