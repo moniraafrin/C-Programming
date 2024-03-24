49401a0261bad62f48dfc19041973d4fd8f70dfa
Monira Akter <158304777+moniraafrin@users.noreply.github.com>
2024-03-24 19:51:35 +0600

    pattern

---
+++ lab-report-184-1.3.c
@@ -0,0 +1,16 @@
+/*1.3 Print this pattern according to your id ex. 23203001 -> 023203001
+0 2 3
+2 0 3
+0 0 1*/
+//Mossamot Monira Akter
+//ID: 23303121
+//Serial: 05
+#include<stdio.h>
+int main()
+{
+    printf("Mossamot Monira Akter \nID: 23303121\nSerial: 05\n");
+    printf("...................................................\n");
+    printf("023\n203\n001\n");
+
+    return 0;
+}
