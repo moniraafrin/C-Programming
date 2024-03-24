e0d7986752326e5be80f37e5e5cf2394ba9538b9
Monira Akter <158304777+moniraafrin@users.noreply.github.com>
2024-03-24 19:40:12 +0600

    print name,id,dept,serial

---
+++ lab-report-184-1.2.c
@@ -0,0 +1,14 @@
+//1.2 Write a program to print your name,ID,serial,department.
+
+//Mossamot Monira Akter
+//ID: 23303121
+//Serial: 05
+#include<stdio.h>
+int main()
+{
+    printf("Mossamot Monira Akter\nID: 23303121\nSerial: 05\n");
+    printf("...................................................\n");
+    printf("Name: Mossamot Monira Akter\nID: 23303121\nSerial: 05\nDepartment: CSE\n");
+
+    return 0;
+}
