338e191f60fd0cb49dc1c7ae596971623bf17c43
Monira Akter <158304777+moniraafrin@users.noreply.github.com>
2024-03-24 19:55:49 +0600

    print date and time on my pc and laptop

---
+++ lab-report-184-2.1.c
@@ -0,0 +1,13 @@
+//2.1 Print the current operating system date and time of your PC or Laptop.
+//Mossamot Monira Akter
+//ID: 23303121
+//Serial: 05
+#include<stdio.h>
+int main()
+{
+    printf("Mossamot Monira Akter\nID: 23303121\nSerial: 05\n");
+    printf(".........................................................\n");
+    printf("Date:23.03.2024\nTime: 12:00 AM\n");
+
+    return 0;
+}
