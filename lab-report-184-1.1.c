2f33e03de0dde7b296544a6bb27e057891f4a421
Monira Akter <158304777+moniraafrin@users.noreply.github.com>
2024-03-24 19:38:55 +0600

    print the index page

---
+++ lab-report-184-1.1.c
@@ -0,0 +1,30 @@
+//1.1 Draw(print) the index page.
+//Mossamot Monira Akter
+//ID: 23303121
+//Serial: 05
+#include<stdio.h>
+int main()
+{
+           printf("Mossamot Monira Akter\nID: 23303121\nSerial: 05\n");
+           printf("..................................................\n");
+           printf("Index of Lab Report\n");
+           printf("Spring Semester 2024\n");
+           printf("Course Instructor: Md. Nazir Ahmed\n");
+           printf("Course Code: 184\n");
+           printf("Course Name: Programming in C Lab\n");
+           printf("Name:\n");
+           printf("Department:\n");
+           printf("Serial:\n");
+           printf("Section:\n");
+           printf("SL\tDATE\tTITLE\tCOMMENT\tSIGNATURE and DATE\n");
+           printf("01\n");
+           printf("02\n");
+           printf("03\n");
+           printf("04\n");
+           printf("05\n");
+           printf("06\n");
+           printf("07\n");
+           printf("08\n");
+
+           return 0;
+}
