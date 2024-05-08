be7fdfe900d4010c7841b09c2b41bcb3b0d4401a
Monira Akter <158304777+moniraafrin@users.noreply.github.com>
2024-05-08 11:00:29 +0600

    function

---
+++ fucntion_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2, num3;
+
+
+    printf("Sum = %d\nsub = %d\n", sum(6,6,6));
+}
+
+//return_type function_name()
+int sum (int a,int b,int c)
+{
+    return a+b+c;
+    return a+b-c;
+}
