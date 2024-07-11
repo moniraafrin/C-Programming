f48a5c6c452182b71b9d93db54ae7db1935066d5
Monira Akter <158304777+moniraafrin@users.noreply.github.com>
2024-07-11 17:57:10 +0600

    pattern.c
    pattern_16

---
+++ pattern_part_2.6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+     int n,i,j,k;
+     printf("Enter the value of N: ");
+     scanf("%d",&n);
+
+     int count = 1;
+
+     for(i = 1; i <= n; i++)
+     {
+       for(j = 1; j <= i; j++)
+       {
+           printf("%d", count++);
+
+       }
+       printf("\n");
+     }
+
+}
